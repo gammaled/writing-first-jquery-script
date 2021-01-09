8e03c33f3727c5ac9b57238562620a7ae97d02c9
gammaled <guleduni@yahoo.co.uk>
2021-01-09 17:59:07 +0000

    added get elementbyid method to add data into p tags with demo id

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function loadData() {
     $.get('https://ci-swapi.herokuapp.com/api/', function(data, status) {
+        document.getElementById('demo').innerHTML = data;
         console.log("Data: " + data + '/nStatus: ' + status);
     });
 };
