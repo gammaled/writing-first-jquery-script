d025b87a7e3e4720fdfe81b806716eb66fad4d9a
gammaled <guleduni@yahoo.co.uk>
2021-01-13 21:42:39 +0000

    fixed bug that does not show data in console

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
 
-    async function getData() {
-        const response = await fetch('https://ci-swapi.herokuapp.com/api/');
+    async function getData(category) {
+        const response = await fetch(`https://ci-swapi.herokuapp.com/api/${category}`);
         const result = await response.json();
         document.getElementById('demo').innerHTML = JSON.stringify(result);
-    }
-    
+        for (let item of result['results']) {
+            console.log(item.name);
+            return JSON.parse(item.name);
+        };
+    }; 
