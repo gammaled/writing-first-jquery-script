f2869346b4ae231483ab69185e6dab3624de40c2
gammaled <guleduni@yahoo.co.uk>
2021-01-14 20:23:54 +0000

    moved getElementById method into the loop

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,8 @@
     async function getData(category) {
         const response = await fetch(`https://ci-swapi.herokuapp.com/api/${category}`);
         const result = await response.json();
-        document.getElementById('demo').innerHTML = JSON.stringify(result);
         for (let item of result['results']) {
             console.log(item.name);
-            return JSON.parse(item.name);
+            document.getElementById('demo').innerHTML += JSON.stringify(item.name);
         };
     }; 
