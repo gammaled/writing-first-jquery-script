dd927e261de230029698c303670a6b33347afd8c
gammaled <guleduni@yahoo.co.uk>
2021-01-15 14:33:30 +0000

    added OR logical operator to display either name or title of data requested

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 
     async function getData(category) {
-        const response = await fetch(`https://ci-swapi.herokuapp.com/api/${category}`);
+        const response = await fetch(`https://ci-swapi.herokuapp.com/api/${category}/`);
         const result = await response.json();
         for (let item of result['results']) {
-            console.log(item.name);
-            document.getElementById('demo').innerHTML += JSON.stringify(item.name);
+            console.log(item.name || item.title);
+            document.getElementById('demo').innerHTML += JSON.stringify(item.name || item.title);
         };
     }; 
